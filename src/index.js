6329d78e76fb1e65b319d6301f3dafbe66368c72
Ken Anderson <ken@launchbadge.com>
2015-05-31 17:49:55 -0700

    Removed legacy code from index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-// var Crawler = require('./crawler.js');
-// var async = require('async');
-//
-// var spider = new Crawler();
-//
-// async.forever(
-//   function(next){
-//     spider.crawl(function(){
-//       process.nextTick(function(){
-//         next(null);
-//       })
-//     });
-//   },
-//   function(err){
-//     console.log(err);
-//   }
-// );
-
 import config from "config"
 import log from "./log"
 import path from "path"
